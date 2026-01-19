764b5b4ed1b7dbc74816ed3857909931775f5c4e
golubevNikita <vulivercushe.premedu@gmail.com>
2026-01-19 23:10:55 +0300

    continue cosmetic

--- JS/index-hw7.js
+++ JS/index-hw7.js
@@ -1,39 +1,40 @@
 // Задание 1
-const mySting = 'js';
-const myUpperSting = mySting.toUpperCase()
-console.log(myUpperSting)
+const mySting = "js";
+const myUpperSting = mySting.toUpperCase();
+console.log(myUpperSting);
 
 // Задание 2
-const myArray = ['Беги, Форрест, беги!',
-    'Дженни и я были как горох и морковь',
-    'Моя мама говорит дурака видно по его поступкам',
-    '-Гамп, почему ты так быстро все сделал? -Потому что вы приказали',
-    'Чёрт подери, Гамп, кажется, ты гений. Лучшего ответа я ещё не слышал. У тебя IQ случайно не 160? Ты талантище, рядовй Гамп',
-    'Моя мама всегда говорила, что жизнь похожа на коробку шоколадных конфет - никогда не знаешь какую вытянешь',
-    'Мне сказали, что моё ранение тянет на миллион долларов, но похоже, что армия оставила эти деньги себe',
-    'Мама всегда говорила, что о человеке можно судить по его обуви',
-    'Лейтенант Дэн заставил меня вложиться в какую-то фруктовую компанию',
+const myArray = [
+  "Беги, Форрест, беги!",
+  "Дженни и я были как горох и морковь",
+  "Моя мама говорит дурака видно по его поступкам",
+  "-Гамп, почему ты так быстро все сделал? -Потому что вы приказали",
+  "Чёрт подери, Гамп, кажется, ты гений. Лучшего ответа я ещё не слышал. У тебя IQ случайно не 160? Ты талантище, рядовй Гамп",
+  "Моя мама всегда говорила, что жизнь похожа на коробку шоколадных конфет - никогда не знаешь какую вытянешь",
+  "Мне сказали, что моё ранение тянет на миллион долларов, но похоже, что армия оставила эти деньги себe",
+  "Мама всегда говорила, что о человеке можно судить по его обуви",
+  "Лейтенант Дэн заставил меня вложиться в какую-то фруктовую компанию",
 ];
 
-function includesString (arr, str) {
-    const newMyArray = [];
+function includesString(arr, str) {
+  const newMyArray = [];
 
-    for (el of arr) {
-        if (el.toLowerCase().startsWith(str.toLowerCase())) {
-            newMyArray.push(el);
-        } else {
-            continue;
-        };
-    };
-
-    if (newMyArray.length === 0) {
-        return 'Совпадений нет';
+  for (el of arr) {
+    if (el.toLowerCase().startsWith(str.toLowerCase())) {
+      newMyArray.push(el);
     } else {
-        return newMyArray;
-    };
-};
+      continue;
+    }
+  }
 
-console.log(includesString(myArray, 'моя'));
+  if (newMyArray.length === 0) {
+    return "Совпадений нет";
+  } else {
+    return newMyArray;
+  }
+}
+
+console.log(includesString(myArray, "моя"));
 
 // Задание 3
 const myNumber = 32.58884;
@@ -50,55 +51,56 @@ console.log(Math.round(myNumber));
 // Задание 4
 const myArray = [52, 53, 49, 77, 21, 32];
 
-function minElement (arr) {
-    let minValue = maxValue = arr[0];
-    
-    for (el of arr) {
-        if (el < minValue) {
-            minValue = el;
-        };
+function minElement(arr) {
+  let minValue = (maxValue = arr[0]);
 
-        if (el > maxValue) {
-            maxValue = el;
-        };
-    };
+  for (el of arr) {
+    if (el < minValue) {
+      minValue = el;
+    }
 
-    return `Минимальное значение: ${minValue}. максимальное значение: ${maxValue}`;
-};
+    if (el > maxValue) {
+      maxValue = el;
+    }
+  }
+
+  return `Минимальное значение: ${minValue}. максимальное значение: ${maxValue}`;
+}
 
 console.log(minElement(myArray));
 
-/* ---===   ===--- */
 const myArray = [52, 53, 49, 77, 21, 32];
 const minValue = Math.min(...myArray);
 const maxValue = Math.max(...myArray);
 
-console.log(`Минимальное значение: ${minValue}. максимальное значение: ${maxValue}`);
+console.log(
+  `Минимальное значение: ${minValue}. максимальное значение: ${maxValue}`
+);
 
 // Задание 5
-function randomNumberFromOneToTen () {
-    return Math.ceil(Math.random() * 10);
-};
+function randomNumberFromOneToTen() {
+  return Math.ceil(Math.random() * 10);
+}
 
 console.log(randomNumberFromOneToTen());
 
 // Задание 6
-function randomArray (num) {
-    const myArray = [];
+function randomArray(num) {
+  const myArray = [];
 
-    for (i = 1; i <= num / 2; i++) {
-        myArray.push(Math.floor(Math.random() * (num + 1)));
-    };
+  for (i = 1; i <= num / 2; i++) {
+    myArray.push(Math.floor(Math.random() * (num + 1)));
+  }
 
-    return myArray;
-};
+  return myArray;
+}
 
 console.log(randomArray(9));
 
 // Задание 7
-function randomNumberRange (minnum, maxnum) {
-    return Math.floor(Math.random() * ((maxnum - minnum) + 1)) + minnum;
-};
+function randomNumberRange(minnum, maxnum) {
+  return Math.floor(Math.random() * (maxnum - minnum + 1)) + minnum;
+}
 
 console.log(randomNumberRange(8, 15));
 
@@ -107,11 +109,13 @@ console.log(Date());
 
 // Задание 9
 const currentDate = new Date();
-function daysToMilliseconds (days) {
-    return days * 24 * 60 * 60 * 1000;
+function daysToMilliseconds(days) {
+  return days * 24 * 60 * 60 * 1000;
 }
 
-const convertedDate = new Date(+currentDate + daysToMilliseconds(73)).toString();
+const convertedDate = new Date(
+  +currentDate + daysToMilliseconds(73)
+).toString();
 
 console.log(currentDate.toString());
 console.log(convertedDate);
@@ -119,17 +123,28 @@ console.log(convertedDate);
 // Задание 10
 const currentDate = new Date();
 const settingsObject = {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  day: "numeric",
+  month: "long",
+  year: "numeric",
 };
 
-const daysOfTheWeek = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const daysOfTheWeek = [
+  "Воскресенье",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+];
 
-function getDayandTime (date) {
-    const dateString = `Дата: ${date.toLocaleDateString('ru-RU', settingsObject)} — это ${daysOfTheWeek[date.getDay()]}.`;
-    const timeString = `Время: ${date.toLocaleTimeString('ru-RU')}`;
-    return dateString + '\n' + timeString;
-};
+function getDayandTime(date) {
+  const dateString = `Дата: ${date.toLocaleDateString(
+    "ru-RU",
+    settingsObject
+  )} — это ${daysOfTheWeek[date.getDay()]}.`;
+  const timeString = `Время: ${date.toLocaleTimeString("ru-RU")}`;
+  return dateString + "\n" + timeString;
+}
 
 console.log(getDayandTime(currentDate));
--- JS/index-hw8.js
+++ JS/index-hw8.js
@@ -1,117 +1,110 @@
 // Задание 1
 const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 },
+  { name: "Глеб", age: 29 },
+  { name: "Анна", age: 17 },
+  { name: "Олег", age: 7 },
+  { name: "Оксана", age: 47 },
 ];
 
 console.log(people.sort((a, b) => a.age - b.age));
 
 // Задание 2
 const people = [
-    {name: 'Глеб', gender: 'male'},
-    {name: 'Анна', gender: 'female'},
-    {name: 'Олег', gender: 'male'},
-    {name: 'Оксана', gender: 'female'},
+  { name: "Глеб", gender: "male" },
+  { name: "Анна", gender: "female" },
+  { name: "Олег", gender: "male" },
+  { name: "Оксана", gender: "female" },
 ];
 
 function isPositive(item) {
-    return item >= 0;
-};
+  return item >= 0;
+}
 
 function isMale(item) {
-    return item.gender === 'male';
-};
+  return item.gender === "male";
+}
 
 function filter(arr, ruleFunction) {
-    const newArr = [];
+  const newArr = [];
 
-    for (el of arr) {
-        if (ruleFunction(el)) {
-            newArr.push(el);
-        };
-    };
-    return newArr;
-};
+  for (el of arr) {
+    if (ruleFunction(el)) {
+      newArr.push(el);
+    }
+  }
+  return newArr;
+}
 
 console.log(filter([3, -4, 1, 9], isPositive));
 console.log(filter(people, isMale));
 
-/* новый массив, образованный по результатам вызова метода map, как будто бы всегда будет равен длине
-массива, в котором этот метод вызывали, и в любом случае вернёт значение какого-либо выражения - 
-инструкции по типу continue ему не подходят (я пытался, честно) - такии образом, в итоговом массиве
-среди элементов, выполнивших условия функции-регулятора, будет undefined - результат, который
-получен из-за невыполнения итеррируемым элементом условий функции-регулятора */
-
 const people = [
-    {name: 'Глеб', gender: 'male'},
-    {name: 'Анна', gender: 'female'},
-    {name: 'Олег', gender: 'male'},
-    {name: 'Оксана', gender: 'female'},
+  { name: "Глеб", gender: "male" },
+  { name: "Анна", gender: "female" },
+  { name: "Олег", gender: "male" },
+  { name: "Оксана", gender: "female" },
 ];
 
 function isPositive(item) {
-    return item >= 0;
-};
+  return item >= 0;
+}
 
 function isMale(item) {
-    return item.gender === 'male';
-};
+  return item.gender === "male";
+}
 
 function filter(arr, ruleFunction) {
-    const result = arr.map((el) => {
-        if (ruleFunction(el)) {
-            return el;
-        };
-    });
+  const result = arr.map((el) => {
+    if (ruleFunction(el)) {
+      return el;
+    }
+  });
 
-    const newArr = result.filter((el) => el !== undefined); // зато через map
+  const newArr = result.filter((el) => el !== undefined);
 
-    return newArr;
-};
+  return newArr;
+}
 
 console.log(filter([3, -4, 1, 9], isPositive));
 console.log(filter(people, isMale));
 
 // Задание 3
 function printDate(interval, duration) {
+  function callMessage() {
+    console.log(new Date().toString());
+  }
 
-    function callMessage () {
-        console.log(new Date().toString());
-    };
-    
-    const intervalID = setInterval(callMessage, interval * 1000);
-    
-    setTimeout(() => {
-        console.log(`${duration} сек прошло`);
-        clearInterval(intervalID);
-    }, duration * 1000);
-};
+  const intervalID = setInterval(callMessage, interval * 1000);
+
+  setTimeout(() => {
+    console.log(`${duration} сек прошло`);
+    clearInterval(intervalID);
+  }, duration * 1000);
+}
 
 printDate(3, 30);
 
 // Задание 4
 function delayForSecond(callback) {
-    setTimeout(callback, 1000);
+  setTimeout(callback, 1000);
 }
 
 delayForSecond(function () {
-    console.log('Привет, Глеб!');
+  console.log("Привет, Глеб!");
 });
 
 // Задание 5
 function delayForSecond(cb) {
-    setTimeout(() => {
-        console.log('Прошла одна секунда');
-        if (cb) {
-            cb();
-        };
-    }, 1000);
-};
+  setTimeout(() => {
+    console.log("Прошла одна секунда");
+    if (cb) {
+      cb();
+    }
+  }, 1000);
+}
 
-function sayHi (name) {
-    console.log(`Привет, ${name}!`);
-};
+function sayHi(name) {
+  console.log(`Привет, ${name}!`);
+}
 
-delayForSecond(() => sayHi('Глеб'));
+delayForSecond(() => sayHi("Глеб"));
